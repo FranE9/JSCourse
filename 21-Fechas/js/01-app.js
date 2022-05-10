71eb0850fd208a721c9765f513105b8d4a8f8b11
JoseCoFR7 <00105018@uca.edu.sv>
2022-05-10 00:04:03 -0600

    21- Finished

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,15 @@
+const diaHoy = new Date();
+
+let valor;
+
+valor = diaHoy;
+
+valor=diaHoy.getFullYear();
+valor=diaHoy.getMonth();
+valor=diaHoy.getHours();
+valor=diaHoy.getMilliseconds();
+valor=diaHoy.setFullYear(2010);
+
+
+
+console.log(valor);
